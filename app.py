f15b1a8adde9477f0c40b42a812bd391f03be574
m-pihlak <pihlakmer@gmail.com>
2024-08-19 00:48:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return str(HTMLElement(tag="h1", innerHTML="Hey"))
+    return str(HTMLElement(
+        tag="h1", 
+        innerHTML="Hey",
+        style="color: cyan;",
+        ))
