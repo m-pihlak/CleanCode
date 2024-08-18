e4ffe10d7aa20b111229870a6d544bdf906ad4d9
m-pihlak <pihlakmer@gmail.com>
2024-08-19 00:49:09 +0300

    Added comments, encapsulation and arguments

--- objects/html_element.py
+++ objects/html_element.py
@@ -2,10 +2,27 @@ class HTMLElement:
 
     def __init__(self, 
                  tag: str,
-                 innerHTML: str = ""):
-        self.tag: str = tag
-        self.innerHTML = innerHTML
+                 innerHTML: str = "",
+                 **kwargs):
+        self.__tag: str = tag
+        self.__innerHTML: str = innerHTML
+        self.__arguments: dict = kwargs
     
-
+    """
+    Returns argument key-value pairs as space separated
+    strings in the form: key = "value"
+    Example:
+        argument:
+            key: "style"
+            value: "color: cyan;"
+        string:
+            "style = \"color: cyan;\""
+    """
+    def args(self):
+        return " ".join([
+            f"{key} = \"{value}\""
+            for key, value in self.__arguments.items()
+        ])
+    
     def __str__(self):
-        return f"<{self.tag}>{self.innerHTML}</{self.tag}>"
+        return f"<{self.__tag} {self.args()}>{self.__innerHTML}</{self.__tag}>"
